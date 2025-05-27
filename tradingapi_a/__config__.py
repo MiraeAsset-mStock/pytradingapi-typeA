2735562a07746ee0c15b5f81b0959ecc7221c8d6
kailashmirae <kailash.finquest@miraeassetcm.com>
2025-05-27 15:04:51 +0530

    Update __config__.py

--- tradingapi_a/__config__.py
+++ tradingapi_a/__config__.py
@@ -1,4 +1,4 @@
-API_KEY="2axlHl4XWZoO2mSToElejcTOkc09QSxHP9YxrywofSo@"  # Replace with your actual API key
+API_KEY="2axlHl4XWZoO2mSToEle"  # Replace with your actual API key
 default_root_uri= "https://api.mstock.trade/"
 routes= {
         "login": "openapi/typea/connect/login",
